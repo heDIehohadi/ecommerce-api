2516699e5be0632b650b06f462ecf08baca1baac
HeDieh <ohadihedi@gmail.com>
2026-06-17 20:33:14 +0330

    feat:improve django modeladmin configuration

--- product/admin.py
+++ product/admin.py
@@ -1,19 +1,33 @@
 from django.contrib import admin
 from .models import (
     StoreCategory,
-    StoreProduct,
     StoreComment,
+    StoreProduct,
     StoreProductImage,
 )
+
+
 @admin.register(StoreCategory)
 class AdminStoreCategory(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
+    ordering = ('name',)
+
+
 @admin.register(StoreComment)
 class AdminStoreComment(admin.ModelAdmin):
-    pass
+    list_display = ('user', 'product', 'rating')
+    list_filter = ('rating',)
+    ordering = ('-id',)
+
+
 @admin.register(StoreProduct)
 class AdminStoreProduct(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'price', 'sku', 'category')
+    search_fields = ('name', 'sku')
+    list_filter = ('category',)
+    ordering = ('-id',)
+
+
 @admin.register(StoreProductImage)
 class AdminProductImage(admin.ModelAdmin):
-    pass
+    list_display = ('product', 'id')
