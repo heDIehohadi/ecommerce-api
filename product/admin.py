0755d030b6f088d5cc3723882fd68bd99ed58ef3
HeDieh <ohadihedi@gmail.com>
2026-06-17 20:15:42 +0330

    register products model in adminpanel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import (
+    StoreCategory,
+    StoreProduct,
+    StoreComment,
+    StoreProductImage,
+)
+@admin.register(StoreCategory)
+class AdminStoreCategory(admin.ModelAdmin):
+    pass
+@admin.register(StoreComment)
+class AdminStoreComment(admin.ModelAdmin):
+    pass
+@admin.register(StoreProduct)
+class AdminStoreProduct(admin.ModelAdmin):
+    pass
+@admin.register(StoreProductImage)
+class AdminProductImage(admin.ModelAdmin):
+    pass
